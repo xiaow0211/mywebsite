99dcdd20b1d2cff399339a7766e4473738d7a74d
xiaow0211 <31690424+xiaow0211@users.noreply.github.com>
2019-10-02 21:16:21 +0800

    Add files via upload

--- test/video/main.js
+++ test/video/main.js
@@ -1,11 +1,32 @@
 var video = document.getElementById('video');
 var audio = document.getElementById('audio');
-video.onclick = function(){
-	video.play()
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+canvas.height = screen.availHeight;
+canvas.width = screen.availWidth;
+var i = 0;
+video.addEventListener('canplay', function() {
+    window.onclick = function() {
+        video.play();
+        play();
+
+    }
+}, false)
+
+function play() {
+    
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        raf(play)
+    
 }
-video.onended = function(){
-	video.currentTime = 22;
-	video.play()
-	audio.volume = 0.2;
-	audio.play()
+video.onended = function() {
+    video.play();
+    video.currentTime = 21;
+    if (i == 0) {
+        audio.volume = 0.2;
+        audio.play();
+    }
+    i += 1;
 }
+
+var raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) { window.setTimeout(callback, 1000 / 60); };
